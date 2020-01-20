032657c4bb291fc2c9329cb592b6bb937b9947b2
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-20 17:53:36 +0000

    Update server.js
    * Setup route for all users.
    * Db connection was established.

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,28 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const PORT = 4000;
 const seedData = require("./seed.js");
-
+const { readAllUsers } = require("./database.js");
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect('mongodb://127.0.0.1:27017/Habits', { useNewUrlParser: true });
 
-const connection = mongoose.connection;
-
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully")
-})
+// connection.once('open', function() {
+//     console.log("MongoDB database connection established successfully")
+// })
 
 //seed data
 app.get('/seed', (req, res) => {
     seedData();
     res.send("Data seeded.");
 })
+
+// Get all users
+app.get("/allUsers", (req, res) => {
+    let users = readAllUsers();
+    res.json(users);
+});
+
 //get one record
 
 //save new user
