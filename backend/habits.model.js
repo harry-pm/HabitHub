682a60183fc3e570cb90430c1c44782aa4df216e
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-20 13:01:11 +0000

    Setup Mongoose model

---
+++ backend/habits.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+let userSchema = new schema({
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+})
+
+let habitSchema = new schema({
+    name: {
+        type: String
+    },
+    completed: {
+        type: [Boolean]
+    },
+    streak: {
+        type: Number
+    }
+})
+
+let recordSchema = new schema({
+    user: userSchema,
+    habits: [habitSchema]
+});
+
+module.exports = mongoose.model("Record", recordSchema)
