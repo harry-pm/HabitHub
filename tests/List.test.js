3af7b26329434cb113ae3e372be6c0b08c299e16
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-21 11:01:20 +0000

    Wrote failing test to see if the list of habits get displayed

--- tests/List.test.js
+++ tests/List.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from '../src/components/List';
+import { shallow } from 'enzyme';
 
 describe('List', () => {
 
@@ -8,5 +9,24 @@ describe('List', () => {
       const div = document.createElement('div');
       ReactDOM.render(<List />, div);
       ReactDOM.unmountComponentAtNode(div);
+   });
+
+  // Checks if a list of habits is rendered in the 
+  it("Renders a list of habits", () => {
+    const wrapper = shallow(<List />);
+    const dummyData = [{
+      name: "washing",
+      streak: 3,
+      completed: [true],
+      lastCompleted: "19 / 01 / 2020"
+    },
+    {
+      name: "pushups",
+      streak: 100,
+      completed: [true, true, false],
+      lastCompleted: "19 / 01 / 2020"
+    }];
+    wrapper.setProps({ habits: dummyData });
+    expect(wrapper.find(".list")).to.not.have.lengthOf(0);
   });
 })
