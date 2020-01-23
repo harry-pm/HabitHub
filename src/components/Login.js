2ec9c21417063f992833afecc6e154457cbcec78
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-23 16:21:27 +0000

    Altered placeholder text and injecting Bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,9 +7,9 @@ export default function Login(props) {
             <h1>Habit Hub</h1>
             {/* onSubmit verify function passed from props to be added */}
             <form onSubmit={props.handleLogin}>
-                <input type="text" name="username" placeholder="enter username" onChange={props.handleUsername}></input>
-                <input type="text" name="password" placeholder="enter password"onChange={props.handlePassword}></input>
-                <input type="submit" value="submit" ></input>
+                <input type="text" name="username" placeholder="Enter username" onChange={props.handleUsername}></input>
+                <input type="text" name="password" placeholder="Enter password" onChange={props.handlePassword}></input>
+                <input type="submit" className="loginButton" value="Log in" className="btn btn-primary btn-lg"></input>
             </form>
         </div>
     )
