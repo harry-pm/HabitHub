ae0a3a7e70948ee76bde78403b716df47ec8b6fd
Josh Torbett <josh.torbett@getfutureproof.co.uk>
2020-01-23 23:29:26 +0000

    fully test readUserHabits/:id route, streaking and reset of cpmpleted values

--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -14,7 +14,7 @@ describe('API Routes', function() {
           chai.request(server)
           .get('/readAllUsers')
           .end(function(err, res) {
-            //   console.log(res.body)
+          console.log(res.body)
           res.should.have.status(200);
           res.should.be.json; // jshint ignore:line
           res.body.should.be.a('array');
@@ -44,30 +44,56 @@ describe('API Routes', function() {
           res.body[0].should.have.property('completed');
           res.body[0].completed.should.be.a('array')
           res.body[0].completed[0].should.equal(false);
+          res.body[0].completed[1].should.equal(false);
+          res.body[0].completed[2].should.equal(false);
           res.body[0].should.have.property('lastCompleted');
           res.body[0].lastCompleted.should.equal('2020-01-21T00:00:00.000Z');
           done();
           });
         });
 
-        it('should return the habits for 1 user, updating streak and completed values', function(done) {
+        it('should return the habits for 1 user, increase streak and completed to false', function(done) {
             chai.request(server)
             .get('/readUserHabits/5e29fb7796f816202cdf5047')
             .end(function(err, res) {
-              console.log(res.body)
+              // console.log(res.body)
             res.should.have.status(200);
             res.should.be.json; // jshint ignore:line
             res.body.should.be.a('array');
             res.body.length.should.equal(1);
-            // res.body[0].should.have.property('name');
-            // res.body[0].name.should.equal('eating');
-            // res.body[0].should.have.property('streak');
-            // res.body[0].streak.should.equal(0);
-            // res.body[0].should.have.property('completed');
-            // res.body[0].completed.should.be.a('array')
-            // res.body[0].completed[0].should.equal(false);
-            // res.body[0].should.have.property('lastCompleted');
-            // res.body[0].lastCompleted.should.equal('2020-01-21T00:00:00.000Z');
+            res.body[0].should.have.property('name');
+            res.body[0].name.should.equal('exercise');
+            res.body[0].should.have.property('streak');
+            res.body[0].streak.should.equal(8);
+            res.body[0].should.have.property('completed');
+            res.body[0].completed.should.be.a('array')
+            res.body[0].completed[0].should.equal(false);
+            res.body[0].should.have.property('lastCompleted');
+            res.body[0].lastCompleted.should.equal('2020-01-22T00:00:00.000Z');
+            done();
+            });
+          });
+
+          it('should return the habits for 1 user, reset streak and completed to false', function(done) {
+            chai.request(server)
+            .get('/readUserHabits/5e29fb7796f816202cdf5040')
+            .end(function(err, res) {
+              // console.log(res.body)
+            res.should.have.status(200);
+            res.should.be.json; // jshint ignore:line
+            res.body.should.be.a('array');
+            res.body.length.should.equal(2);
+            res.body[1].should.have.property('name');
+            res.body[1].name.should.equal('pushups');
+            res.body[1].should.have.property('streak');
+            res.body[1].streak.should.equal(0);
+            res.body[1].should.have.property('completed');
+            res.body[1].completed.should.be.a('array')
+            res.body[1].completed[0].should.equal(false);
+            res.body[1].completed[1].should.equal(false);
+            res.body[1].completed[2].should.equal(false);
+            res.body[1].should.have.property('lastCompleted');
+            res.body[1].lastCompleted.should.equal('2020-01-19T00:00:00.000Z');
             done();
             });
           });
