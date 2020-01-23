ddb092fc1c8be36360d21ce953c74bce94cd5c3e
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-23 18:47:49 +0000

    Placed drop-down tag between 'p' tags

--- src/components/Add.js
+++ src/components/Add.js
@@ -23,13 +23,14 @@ export default class Add extends Component {
         return (
         <div>
             <input required onChange={this.handleName} type="text" name="add" placeholder="Add a habit..."></input>
-            <p>Frequency per day: </p>
-            <select required onChange={this.handleFrequency}>
+            
+            <p>Frequency per day: <select required onChange={this.handleFrequency}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
-            </select>
+            </select></p>
+            
             <button onClick={this.props.addHabit.bind(this, this.state.name, this.state.frequency)}>Add habit you addict!</button>
         </div>
         )
