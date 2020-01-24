0b14a3bdd5546fea984c186cfc7a4efb34465f5f
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-01-24 12:03:53 +0000

    Logo for habit streak (#16)
    * Deleted unused images
    
    * Installed Path
    
    * Express can now access the image
    
    * The streak logo
    
    * Putting the image next to the streak number

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
 const seedData = require("./seed.js");
-// const seedDataTest = require("./test/seedTest.js")
-
+const seedDataTest = require("./test/seedTest.js")
+const path = require("path");
 const {
     readAllUsers,
     readUser,
@@ -20,6 +20,8 @@ const {
 app.use(cors());
 app.use(bodyParser.json());
 
+// Provides Express with access to the 'Assets' folder within 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 //seed data
 app.get('/seed', (req, res) => {
